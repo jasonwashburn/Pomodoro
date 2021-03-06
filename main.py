70c2324f4e4e43a02c1d76fc23f24d39340852a7
Jason Washburn <wburn@wburn.net>
2021-03-06 07:14:31 -0600

    completed timing logic, reset function, and window focusing

--- main.py
+++ main.py
@@ -12,13 +12,48 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def reset():
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title.config(text='Timer')
+    counter_label.config(text='')
+    reps = 0
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN
+    short_break_sec = SHORT_BREAK_MIN
+    long_break_sec = LONG_BREAK_MIN
+
+    if reps % 2 != 0 and reps < 8:
+        # Work Session
+        focus_window("off")
+        count_down(work_sec)
+        title.config(text="Work", fg=GREEN)
+    elif reps >= 8:
+        # Long Break Session
+        focus_window("on")
+        count_down(long_break_sec)
+        title.config(text="Break", fg=RED)
+    elif reps % 2 == 0 and reps < 8:
+        # Short Break Session
+        focus_window("on")
+        count_down(short_break_sec)
+        title.config(text="Break", fg=PINK)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
 
 def count_down(count):
     count_min = math.floor(count / 60)
@@ -26,28 +61,25 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min:02}:{count_sec:02}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = "✔"
+        work_sessions = reps // 2
+        marks *= work_sessions
+        counter_label.config(text=marks)
 
 
-def start_timer():
-    global reps
-    reps += 1
+# ---------------------------- WINDOW FOCUS ------------------------------- #
 
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-
-    if reps % 2 != 0 and reps < 8:
-        count_down(work_sec)
-        title.config(text="Work", fg=GREEN)
-    elif reps >= 8:
-        count_down(long_break_sec)
-        title.config(text="Break", fg=RED)
-    elif reps % 2 == 0 and reps < 8:
-        count_down(short_break_sec)
-        title.config(text="Break", fg=PINK)
+def focus_window(option):
+    if option == "on":
+        window.deiconify()
+        window.focus_force()
+        window.attributes('-topmost', 1)
+    elif option == "off":
+        window.attributes('-topmost', 0)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -74,11 +106,11 @@ start_button = ttk.Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
 # Reset Button
-reset_button = ttk.Button(text="Reset")
+reset_button = ttk.Button(text="Reset", command=reset)
 reset_button.grid(column=2, row=2)
 
 # Counter Label
-counter_label = Label(text='✔', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 18, "bold"))
+counter_label = Label(text='', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 18, "bold"))
 counter_label.grid(column=1, row=3)
 
 window.mainloop()
